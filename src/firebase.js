782e446a1dfdf5e5829c7f6ad39829664d90f075
Ankita <ankita187panda@gmail.com>
2024-06-29 00:15:52 +0530

    chat setup

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDa2_UWvJ030CqL_38QopqOUxc7PlGXylI",
-  authDomain: "chat-20e41.firebaseapp.com",
-  projectId: "chat-20e41",
-  storageBucket: "chat-20e41.appspot.com",
-  messagingSenderId: "19076649986",
-  appId: "1:19076649986:web:c50fd65a90c47d3d72431b"
+  apiKey: "AIzaSyCcO8Yd_90RlwuP7o_3U4TAGEy1G8qI10U",
+  authDomain: "chat-app-4b562.firebaseapp.com",
+  projectId: "chat-app-4b562",
+  storageBucket: "chat-app-4b562.appspot.com",
+  messagingSenderId: "585844279027",
+  appId: "1:585844279027:web:bcb5504b14341729555986"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
-export const db = getFirestore()
+export const db = getFirestore();
